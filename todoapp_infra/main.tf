897cddc964a76a51da2a046370abbd9fd76b6ccd
MOHD ARIF <mohdarif@MOHDs-MacBook-Air.local>
2025-08-26 01:21:24 +0530

    added a new resource group rg-todoapp2 in main.tf

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -12,6 +12,12 @@ module "resource_group1" {
   resource_group_location = "centralindia"
 }
 
+module "resource_group2" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-todoapp2"
+  resource_group_location = "centralindia"
+}
+
 module "virtual_network" {
   depends_on = [module.resource_group]
   source     = "../modules/azurerm_virtual_network"
